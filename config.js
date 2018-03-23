16b49f6078414145f28c5a5ac7638a56728a9bf1
leo gurning <leogurning@gmail.com>
2018-03-23 23:10:48 +0700

    initial commit kx notif heroku

--- config.js
+++ config.js
@@ -1,12 +1,9 @@
 module.exports = {
     'serverport':2004,
-    'tokenexp': 3600,
+    'tokenexp': 7200,
     'secret': '!$3cret4@uth',
-    'database': 'mongodb://admindb:kaxetadmin@35.200.243.4:27017/kaxetdb',
-    /* 'database': 'mongodb://admindb:kaxetadmin@ds127506.mlab.com:27506/kaxetdb', */
-    /* Google Cloud Storage parameters */
-    'GCLOUD_PROJECT': 'kaxetd',
-    'CLOUD_BUCKET': 'kaxetd',
+    /* 'database': 'mongodb://admindb:kaxetadmin@35.200.243.4:27017/kaxetdb', */
+    'database': 'mongodb://admindb:kaxetadmin@ds127506.mlab.com:27506/kaxetdb',
     /* SMTP Email parameters */
     'email_service': 'Gmail',
     'email_userid': 'cs.kaxet10',
