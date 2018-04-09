dd34f891caf8d8872097c0eff6f34a09298e60be
leo gurning <leogurning@gmail.com>
2018-04-09 20:14:09 +0700

    update config and css template

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
     'serverport':2004,
-    'tokenexp': 7200,
+    'tokenexp': 72000,
     'secret': '!$3cret4@uth',
-    /*'database': 'mongodb://admindb:kaxetadmin@35.200.243.4:27017/kaxetdb', */
-    'database': 'mongodb://admindb:kaxetadmin@ds127506.mlab.com:27506/kaxetdb',
+    'database': 'mongodb://admindb:kaxetadmin@35.200.243.4:27017/kaxetdb',
+    // 'database': 'mongodb://admindb:kaxetadmin@ds127506.mlab.com:27506/kaxetdb',
     /* SMTP Email parameters */
     'email_service': 'Gmail',
     'email_userid': 'cs.kaxet10',
