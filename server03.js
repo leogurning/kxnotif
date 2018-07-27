4c6606a3d9729d28692bae9457db7f87d1f168f9
leo gurning <leogurning@gmail.com>
2018-07-27 18:01:50 +0700

    set1 config setting for kaxet notif production

--- server03.js
+++ server03.js
@@ -8,7 +8,7 @@ const emailverify = require('./routes/emailverify.js');
 var path = require('path');
 
 const app = express();
-const port = process.env.PORT || process.env.KXNOTIFPRD_SERVICE_PORT || config.serverport;
+const port = process.env.PORT || process.env.KXNOTIFP_SERVICE_PORT || config.serverport;
 
 mongoose.connect(config.database, function(err){
 	if(err){
